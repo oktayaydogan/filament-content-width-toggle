68f4c2be9f7770fa697defa2121606d2ac514645
oktayaydogan <31257020+oktayaydogan@users.noreply.github.com>
2026-01-02 18:05:13 +0000

    Fix styling

--- src/FilamentContentWidthToggleServiceProvider.php
+++ src/FilamentContentWidthToggleServiceProvider.php
@@ -10,11 +10,11 @@ use Filament\Support\Facades\FilamentAsset;
 use Filament\Support\Facades\FilamentIcon;
 use Illuminate\Filesystem\Filesystem;
 use Livewire\Features\SupportTesting\Testable;
+use Oktayaydogan\FilamentContentWidthToggle\Commands\FilamentContentWidthToggleCommand;
+use Oktayaydogan\FilamentContentWidthToggle\Testing\TestsFilamentContentWidthToggle;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Oktayaydogan\FilamentContentWidthToggle\Commands\FilamentContentWidthToggleCommand;
-use Oktayaydogan\FilamentContentWidthToggle\Testing\TestsFilamentContentWidthToggle;
 
 class FilamentContentWidthToggleServiceProvider extends PackageServiceProvider
 {
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -14,9 +14,9 @@ use Filament\Tables\TablesServiceProvider;
 use Filament\Widgets\WidgetsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
+use Oktayaydogan\FilamentContentWidthToggle\FilamentContentWidthToggleServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Oktayaydogan\FilamentContentWidthToggle\FilamentContentWidthToggleServiceProvider;
 
 class TestCase extends Orchestra
 {
